908909d22b9f4293caf36e80e2b16b2ffd6d5b2a
Hack Reactor Students <github@hackreactor.com>
2013-08-16 23:35:01 -0700

    working on dragging to no avail

---
+++ collider.js
@@ -0,0 +1,11 @@
+// function(){
+// gameOptions =
+//   height: 450
+//   width: 700
+//   nEnemies: 30
+//   padding: 20
+
+// gameBoard = d3.select('.container').append('svg:svg')
+//               .attr('width', gameOptions.width)
+//               .attr('height', gameOptions.height)
+// }  
